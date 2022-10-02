5bd4536e060ca5061f8c8391f2aef33a34ab7e73
Aras <aras.tatar@hotmail.com>
2022-10-03 00:15:31 +0200

    get all married person in javascript based on a array of objects

--- test2.js
+++ test2.js
@@ -1,1 +1,25 @@
-console.log('please save my ass bro i\'m in very big trouble my freind');
+// create array of objects with person data
+
+var people = [
+    {name: "John", age: 25, isMarried: false},
+    {name: "Mary", age: 30, isMarried: true},
+    {name: "Peter", age: 28,isMarried: false},
+    {name: "Susan", age: 24, isMarried: true},
+];
+
+
+// create a function that returns a new array of objects with married people use a foreach loop
+
+function marriedPeople(people) {
+    var married = [];
+    people.forEach(function(person) {
+        if (person.isMarried) {
+            married.push(person);
+        }
+    });
+    return married;
+}
+
+console.log(marriedPeople(people));
+
+
